809a287b6dbf71a6ccb5914d07b3844543c3262d
marinevlrg <marine.valorge@gmail.com>
2022-11-08 11:28:55 +0100

    rectif calcul

--- speedometer.php
+++ speedometer.php
@@ -2,11 +2,13 @@
 
 class Speedometer
 {
-    public const MILESKM_CONVERSION = 0.6213711922;
+    public const KMMILES_CONVERSION = 0.621371;
+
+    public const MILESKM_CONVERSION = 1.60934;
 
     public static function KmToMiles(int $km): float
     {
-        return $km * self::MILESKM_CONVERSION;
+        return $km * self::KMMILES_CONVERSION;
     }
 
     public static function MilesToKm(int $miles): float
